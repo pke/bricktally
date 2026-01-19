b6f1da34d23c8ebfd14164af8ce0c32ba65c486f
Philipp Kursawe <phil.kursawe@gmail.com>
2026-01-20 00:53:58 +0100

    feat: click on part zooms the part to a lightbox

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // BrickTally Service Worker
 // Version: 1
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v20260120-005358';
 const STATIC_CACHE = `bricktally-static-${CACHE_VERSION}`;
 const API_CACHE = `bricktally-api-${CACHE_VERSION}`;
 const IMAGE_CACHE = `bricktally-images-${CACHE_VERSION}`;
