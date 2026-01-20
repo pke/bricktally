e195ddec66f2b90481f3839aaf6452b80213bced
Philipp Kursawe <phil.kursawe@gmail.com>
2026-01-20 16:31:42 +0100

    fix: do not show parts and minifigs on homescreen

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // BrickTally Service Worker
 // Version: 1
-const CACHE_VERSION = 'v20260120-161839';
+const CACHE_VERSION = 'v20260120-163142';
 const STATIC_CACHE = `bricktally-static-${CACHE_VERSION}`;
 const API_CACHE = `bricktally-api-${CACHE_VERSION}`;
 const IMAGE_CACHE = `bricktally-images-${CACHE_VERSION}`;
