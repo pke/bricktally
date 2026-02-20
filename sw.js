7f07efe73db15f1492cf8638ddb67110e91475ed
Philipp Kursawe <phil.kursawe@gmail.com>
2026-02-20 18:03:30 +0100

    fix: change delete icon and align history item actions to bottom

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // BrickTally Service Worker
 // Version: 1
-const CACHE_VERSION = 'v20260220-180233';
+const CACHE_VERSION = 'v20260220-180330';
 const STATIC_CACHE = `bricktally-static-${CACHE_VERSION}`;
 const API_CACHE = `bricktally-api-${CACHE_VERSION}`;
 const IMAGE_CACHE = `bricktally-images-${CACHE_VERSION}`;
