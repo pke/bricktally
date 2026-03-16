d5ad17086ae9d82958f58986864bc4c99535b270
Philipp Kursawe <phil.kursawe@gmail.com>
2026-03-16 18:09:53 +0100

    chore: vendor i18next v24 UMD bundle

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // BrickTally Service Worker
 // Version: 1
-const CACHE_VERSION = 'v20260316-164811';
+const CACHE_VERSION = 'v20260316-180953';
 const STATIC_CACHE = `bricktally-static-${CACHE_VERSION}`;
 const API_CACHE = `bricktally-api-${CACHE_VERSION}`;
 const IMAGE_CACHE = `bricktally-images-${CACHE_VERSION}`;
